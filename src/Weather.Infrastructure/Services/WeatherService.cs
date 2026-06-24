30fec2ee7aa5a2f32540bdf6a80f9e53b3a21208
kushal <collabskus@gmail.com>
2026-06-24 08:55:49 -0400

    qualify name

--- src/Weather.Infrastructure/Services/WeatherService.cs
+++ src/Weather.Infrastructure/Services/WeatherService.cs
@@ -46,7 +46,7 @@ internal sealed class WeatherService(
     INwsApiClient nwsClient,
     INeighborhoodWarmer warmer,
     IRequestCoalescer<GridPoint> forecastCoalescer,
-    [FromKeyedServices(ExtrasCoalescerKey)] IRequestCoalescer<GridPoint> extrasCoalescer,
+    [FromKeyedServices(WeatherService.ExtrasCoalescerKey)] IRequestCoalescer<GridPoint> extrasCoalescer,
     IRequestCoalescer<GeoCoordinate> metadataCoalescer,
     IRequestCoalescer<string> alertCoalescer,
     WeatherTelemetry telemetry,
